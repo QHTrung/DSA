3f360bee692345f5388898a056542958f96246d8
QHTrung <qhtrung23072000@gmail.com>
2024-07-19 17:22:41 +0700

    solve TwoSum using hashmap

--- LeetCode/twoSum.js
+++ LeetCode/twoSum.js
@@ -25,20 +25,28 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 
-const nums = [3, 3];
-const target = 6;
+const nums = [2, 7, 11, 15];
+const target = 9;
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  const map = {};
+  for (let index = 0; index < nums.length; index++) {
+    const num = target - nums[index];
+    if (map[num] !== undefined) {
+      return [map[num], index];
     }
+    map[nums[index]] = index;
   }
 };
 console.log(twoSum(nums, target));
