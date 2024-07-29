bd46bd2c539e395caaa37b51cb683312ffda20e6
QHTrung <qhtrung23072000@gmail.com>
2024-07-29 23:30:18 +0700

    solve calcMultipliesOfN, calcSumOfMul, calcSumX^N

--- 1000_Problem/Ex1.js
+++ 1000_Problem/Ex1.js
@@ -7,3 +7,40 @@ const calcSum = (n) => {
   return sum;
 };
 console.log(calcSum(5));
+// Tính S(n) = 1^2 + 2^2 + … + n^2
+
+const calcSum2 = function (n) {
+  let sum = 0;
+  for (let index = 1; index <= n; index++) {
+    sum += index ** 2;
+  }
+  return sum;
+};
+console.log(calcSum2(3));
+
+// Tính S(n) = 1 + ½ + 1/3 + … + 1/n
+const n = 2;
+const calcSum1 = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += 1 / i;
+  }
+  return sum;
+};
+// Tính S(n) = ½ + ¼ + … + 1/2n
+const calcSum3 = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += 1 / (i * 2);
+  }
+  return sum;
+};
+// Tính S(n) = 1 + 1/3 + 1/5 + … + 1/(2n + 1)
+const calcSum4 = (n) => {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += 1 / (i * 2 + 1);
+  }
+  return sum;
+};
+console.log(calcSum3(n));
--- 1000_Problem/Ex2.js
+++ 1000_Problem/Ex2.js
@@ -1,10 +1,32 @@
-// Tính S(n) = 1^2 + 2^2 + … + n^2
+// Tính T(n) = 1 x 2 x 3…x N
 
-const calcSum = function (n) {
-  let sum = 0;
+const calcMultiplies = (n) => {
+  let result = 1;
   for (let index = 1; index <= n; index++) {
-    sum += index ** 2;
+    result *= index;
+  }
+  return result;
+};
+console.log(calcMultiplies(3));
+
+// Tính S(n) = 1 + 1.2 + 1.2.3 + … + 1.2.3….N
+const calcSumOfMul = (n) => {
+  let mul = 1;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    mul *= i;
+    sum += mul;
   }
   return sum;
 };
-console.log(calcSum(3));
+console.log(calcSumOfMul(4));
+
+// Tính S(n) = x + x^2 + x^3 + … + x^n
+const calcSumXhatN = (x, n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += x ** i;
+  }
+  return sum;
+};
+console.log(calcSumXhatN(3, 4));
